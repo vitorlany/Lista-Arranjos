4d718c391d91ec1d690ccb22af3bc596b4d393af
Vitor Lany <vitorlany99@gmail.com>
2022-06-11 23:10:10 -0300

    ex06

--- ex06.c
+++ ex06.c
@@ -2,15 +2,14 @@
 #include <stdio.h>
 
 int contruirVetor(float * vetor, int tamanho);
+void imprimirVetor(float * vetor, int tamanho);
 
 int main() {
     srand(time(NULL));
     int tamanho = 15;
     float vetor[15];
     contruirVetor(&vetor[0], tamanho);
-    for (int i = 0; i < tamanho; i++) {
-        printf("%f\n", vetor[i]);
-    }
+    imprimirVetor(&vetor[0], tamanho);
     return 0;
 }
 
@@ -19,4 +18,10 @@ int contruirVetor(float * retorno, int tamanho) {
         *(retorno + i) = (rand() % (50 - 30 + 1)) + 30.00;
     }
     return 0;
+}
+
+void imprimirVetor(float * vetor, int tamanho) {
+    for (int i = 0; i < tamanho; i++) {
+        printf("%f\n", *(vetor + i));
+    }
 }
