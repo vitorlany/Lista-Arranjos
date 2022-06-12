ee79ff404e3329a27715fa101f5558f4010aa0ab
Vitor Lany <vitorlany99@gmail.com>
2022-06-11 22:49:25 -0300

    ex06

--- ex06.c
+++ ex06.c
@@ -1,28 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void contruindoVetor(float * vetor[], int tamanho);
-
-// Incompleta, nao consegui descobrir o erro na compilacao
+int contruirVetor(float * vetor, int tamanho);
 
 int main() {
+    srand(time(NULL));
     int tamanho = 15;
     float vetor[15];
-
-	contruindoVetor(&vetor, tamanho);
-	
-    for (int i = 0; i < 15; i++)
-    {
-        printf("Numero: %f\n", vetor[i]);
+    contruirVetor(&vetor[0], tamanho);
+    for (int i = 0; i < tamanho; i++) {
+        printf("%f\n", vetor[i]);
     }
-    
     return 0;
 }
 
-void contruindoVetor(float * vetor[], int tamanho) {
-	for (int i = 0; i < tamanho; i++) {
-        float number = (rand() % (50 - 30 + 1)) + 30.00;
-        //*vetor[i] = number;
-		printf("%.0f %.0f\n", *vetor[i], number);
+int contruirVetor(float * retorno, int tamanho) {
+    for (int i = 0; i < tamanho; i++) {
+        *(retorno + i) = (rand() % (50 - 30 + 1)) + 30.00;
     }
+    return 0;
 }
--- ex07.c
+++ ex07.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 
 int main() {
+    srand(time(NULL));
+    int numero = (rand() % (50 - 15 + 1)) + 15;
     
     return 0;
 }
