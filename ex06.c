cd09659006e16a8f97d87b2438f04d23f590a056
Vitor Lany <vitorlany99@gmail.com>
2022-06-11 21:31:04 -0300

    ex06 (incompleto)

--- ex06.c
+++ ex06.c
@@ -1,6 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void contruindoVetor(float * vetor[], int tamanho);
+
+// Incompleta, nao consegui descobrir o erro na compilacao
+
 int main() {
+    int tamanho = 15;
+    float vetor[15];
+
+	contruindoVetor(&vetor, tamanho);
+	
+    for (int i = 0; i < 15; i++)
+    {
+        printf("Numero: %f\n", vetor[i]);
+    }
+    
     return 0;
+}
+
+void contruindoVetor(float * vetor[], int tamanho) {
+	for (int i = 0; i < tamanho; i++) {
+        float number = (rand() % (50 - 30 + 1)) + 30.00;
+        //*vetor[i] = number;
+		printf("%.0f %.0f\n", *vetor[i], number);
+    }
 }
